f7570b38781b5cc9046a193b823a41400a1a2255
Alberto Valenzuela <avalen28@gmail.com>
2023-04-25 21:08:10 +0200

    adds about first structure

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const About = () => {
-  return <div>about view</div>;
+  return (
+    <div className="about-continer">
+      <div className="about-header">
+        <img src="" alt="avatar" />
+        <article>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+          tempora perspiciatis quibusdam reiciendis exercitationem ad ipsum,
+          earum nesciunt consequuntur, dolor magnam libero cumque omnis dolores
+          tempore ea qui, harum necessitatibus?
+        </article>
+      </div>
+      <div className="about-hobbies">
+        <div className="block">
+          <img src="" alt="block-stuff" />
+          <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+            provident repudiandae vitae cum nihil autem.
+          </article>
+        </div>
+        <div className="block">
+          <img src="" alt="block-stuff" />
+          <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+            provident repudiandae vitae cum nihil autem.
+          </article>
+        </div>
+        <div className="block">
+          <img src="" alt="block-stuff" />
+          <article>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+            provident repudiandae vitae cum nihil autem.
+          </article>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
