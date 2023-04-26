721a77178a2bdab61361d1d70d89f1d78ca27ec8
Alberto Valenzuela <avalen28@gmail.com>
2023-04-26 18:30:36 +0200

    adds about and contact structure

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -4,7 +4,6 @@ const About = () => {
   return (
     <div className="about-continer">
       <div className="about-header">
-        <img src="" alt="avatar" />
         <article>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
           tempora perspiciatis quibusdam reiciendis exercitationem ad ipsum,
@@ -12,27 +11,12 @@ const About = () => {
           tempore ea qui, harum necessitatibus?
         </article>
       </div>
-      <div className="about-hobbies">
-        <div className="block">
+      <div className="about-stack">
+        <div className="frontend-stack">
           <img src="" alt="block-stuff" />
-          <article>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-            provident repudiandae vitae cum nihil autem.
-          </article>
         </div>
-        <div className="block">
+        <div className="baackend-stack">
           <img src="" alt="block-stuff" />
-          <article>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-            provident repudiandae vitae cum nihil autem.
-          </article>
-        </div>
-        <div className="block">
-          <img src="" alt="block-stuff" />
-          <article>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-            provident repudiandae vitae cum nihil autem.
-          </article>
         </div>
       </div>
     </div>
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
-  return <div>contact view</div>;
+  return (
+    <div className="contact-container">
+      <h3>Contact with me</h3>
+      <div className="contact-info">
+        <img src="" alt="LinkedIn" />
+        <Link>https://www.linkedin.com/in/alberto-valenzuela-munoz/</Link>
+      </div>
+      <div className="contact-info">
+        <img src="" alt="GitHub" />
+        <Link>https://github.com/avalen28</Link>
+      </div>
+      <div className="contact-info">
+        <img src="" alt="gmail" />
+        <p>avalen28@gmail.com</p>
+      </div>
+      <div className="contact-info">
+        <img src="" alt="phone" />
+        <p>+34 665-611-219</p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
