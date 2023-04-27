9623fdbcb8f2de4e2b16758a1b06d201095fa6d6
Alberto Valenzuela <avalen28@gmail.com>
2023-04-27 19:40:19 +0200

    Adds images and first about formal structure. WIP

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,17 +5,25 @@ const About = () => {
     <div className="about-continer">
       <div className="about-header">
         <article>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-          tempora perspiciatis quibusdam reiciendis exercitationem ad ipsum,
-          earum nesciunt consequuntur, dolor magnam libero cumque omnis dolores
-          tempore ea qui, harum necessitatibus?
+          For the last 8+ years I've been working as a bulding engineer for the
+          city hall of Barcelona, reviewing amazing projects from small
+          restaurants to Vall d'Hebron Hospital. <br />
+          <br />
+          The other field that I've always been passionate about is tech, and
+          now I'm changing careers to work as a full stack web developer.
+          <br />
+          <br />
+          Architecture and coding are very similar, both need a solid structure,
+          a beautiful look and lots of functionalities so... let's build
+          something together!
         </article>
       </div>
       <div className="about-stack">
         <div className="frontend-stack">
-          <img src="" alt="block-stuff" />
+          <img src="/images/html-5.png" alt="html5" />
+          <img src="/images/css3.png" alt="html5" />
         </div>
-        <div className="baackend-stack">
+        <div className="backend-stack">
           <img src="" alt="block-stuff" />
         </div>
       </div>
