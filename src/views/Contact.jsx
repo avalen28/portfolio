db91ef7d4452e5d594f16af94217a92772b723db
Alberto Valenzuela <avalen28@gmail.com>
2023-05-01 21:20:38 +0200

    Corrects points for deploy

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
